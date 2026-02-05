c9e1cb99195afd7940b61aba11881f27142f75a7
Julia Oliveira <141797663+Oliveirajuu@users.noreply.github.com>
2026-02-05 18:01:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+# Configurando o Flask para servir a pasta 'static'
+app = Flask(__name__, static_folder='static')
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('index.html')  # A página index.html será servida quando a raiz for acessada
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True)  # Rodando o servidor local em modo debug
+
 
